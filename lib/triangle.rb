2f4b64f87fd28c7cd31b11e165da6bd1be9abd49
Adjoa Ntoso <adjoaan@gmail.com>
2019-08-14 00:47:44 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -2,9 +2,6 @@ require 'pry'
 class Triangle
   def initialize(side_1, side_2, side_3)
     @triangle_sides = []
-    @side_1 = side_1
-    @side_2 = side_2
-    @side_3 = side_3
     @triangle_sides << side_1
     @triangle_sides << side_2
     @triangle_sides << side_3
@@ -24,21 +21,64 @@ class Triangle
   end
 
   def kind
-      if @side_1 == 0 || @side_2 == 0 || @side_3 == 0
-        raise TriangleError
-      elsif @side_1 <0 || @side_2 <0 || @side_3 <0
-        raise TriangleError
-      elsif @side_1 != @side_2
-      elsif  @side_1 == @side_2 && @side_2 == @side_3
+    if valid?
+      if @triangle_sides.uniq.length == 1
         return :equilateral
-      elsif @side_1 == @side_2 || @side_2 == @side_3 || @side_1 == @side_3
+      elsif @triangle_sides.uniq.length == 2
         return :isosceles
-      elsif @side_1 != @side_2 && @side_2 != @side_3
+      else
         return :scalene
-#elsif conditinal
-#error
       end
+    else
+      raise TriangleError
     end
+  end
+end
+
 class TriangleError < StandardError
+
 end
-end
+# require 'pry'
+# class Triangle
+#   def initialize(side_1, side_2, side_3)
+#     @triangle_sides = []
+#     @side_1 = side_1
+#     @side_2 = side_2
+#     @side_3 = side_3
+#     @triangle_sides << side_1
+#     @triangle_sides << side_2
+#     @triangle_sides << side_3
+#   end
+#
+#   def valid?
+#     sum_one_two = @triangle_sides[0] + @triangle_sides[1]
+#     sum_one_three = @triangle_sides[0] + @triangle_sides[2]
+#     sum_two_three = @triangle_sides[1] + @triangle_sides[2]
+#
+#     if (@triangle_sides.none? {|side| side <= 0}) &&
+#       (sum_one_two > @triangle_sides[2] && sum_one_three > @triangle_sides[1] && sum_two_three > @triangle_sides[0])
+#       return true
+#     else
+#       return false
+#     end
+#   end
+#
+#   def kind
+#       if @side_1 == 0 || @side_2 == 0 || @side_3 == 0
+#         raise TriangleError
+#       elsif @side_1 <0 || @side_2 <0 || @side_3 <0
+#         raise TriangleError
+#       elsif @side_1 != @side_2
+#       elsif  @side_1 == @side_2 && @side_2 == @side_3
+#         return :equilateral
+#       elsif @side_1 == @side_2 || @side_2 == @side_3 || @side_1 == @side_3
+#         return :isosceles
+#       elsif @side_1 != @side_2 && @side_2 != @side_3
+#         return :scalene
+# #elsif conditinal
+# #error
+#       end
+#     end
+# class TriangleError < StandardError
+# end
+# end
